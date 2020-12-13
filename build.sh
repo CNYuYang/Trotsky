2ac957a2570ad49beb0ba68177ae5d3d54134ed8
余阳 <744727849@qq.com>
2020-12-14 02:27:13 +0800

    [feature]编写构建脚本

---
+++ build.sh
@@ -0,0 +1,24 @@
+#！/bin/sh
+
+echo "➡️ 检查是否安装gradle"
+
+if ! [ -x "$(command -v gradle)" ]; then
+  echo 'Error: gradle is not installed.' >&2
+  exit 1
+fi
+
+gradle -v
+
+echo "➡️ 删除之前构建内容"
+
+rm -rf build
+
+echo "➡️ 获取最新源码"
+
+git pull
+
+echo "➡️ 开始构建"
+
+gradle build -Dquarkus.package.type=native -Dquarkus.native.additional-build-args=-H:ResourceConfigurationFiles=resources-config.json
+
+
